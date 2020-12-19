1dbd5e335029cbb74ba99bc4726b9d8776c1ecae
Abdul Rauf <ar.rauf@gmail.com>
2020-12-19 17:00:30 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Drawer } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
-// import AppDrawer from './components/drawer/Drawer';
 
 function App() {
   return (
     <div className="App">
-      {/* <AppDrawer /> */}
       <Header />
     </div>
   );
